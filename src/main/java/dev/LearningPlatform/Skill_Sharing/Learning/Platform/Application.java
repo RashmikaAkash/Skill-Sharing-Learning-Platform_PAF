a40cb979756196002531cc5b3ebff6c583fb59a2
RashmikaAkash <155883575+RashmikaAkash@users.noreply.github.com>
2025-03-20 01:40:00 +0530

    Reinitialized repository and pulled latest changes

---
+++ src/main/java/dev/LearningPlatform/Skill_Sharing/Learning/Platform/Application.java
@@ -0,0 +1,22 @@
+package dev.LearningPlatform.Skill_Sharing.Learning.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// A simple public endpoint that returns "Hello world"
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello world";
+	}
+}
+
+
